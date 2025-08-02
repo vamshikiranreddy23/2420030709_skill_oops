0ffd1c69e4017dc2d6d5715d6b782d4a521b5b17
vamshikiranreddy23 <thipireddyvamshikiranreddy@gmail.com>
2025-08-02 09:39:48 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,13 @@
+package string_builder;
+
+public class wrapper {
+	public static void main(String args[])
+	{
+		Integer a=554;
+		Boolean b=true;
+		double d=9.876d;
+		int c=10;
+		System.out.println(a.toString().length());
+		System.out.println(b.toString().length());
+	} 
+}
